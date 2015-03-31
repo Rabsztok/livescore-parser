77e2f75fe505c792e4be77bc349abf33db7f0afd
rabsztok <rabsztok@gmail.com>
2015-03-31 10:46:49 +0200

    custom time offset

--- lib/livescore-parser/parser.rb
+++ lib/livescore-parser/parser.rb
@@ -5,7 +5,7 @@ module LivescoreParser
 
     def initialize
       @agent = Mechanize.new
-      @offset = Time.new.utc_offset + (7 * 3600)
+      @offset = Time.new.utc_offset + (5 * 3600)
       cookie = Mechanize::Cookie.new('tz', (@offset / 3600).to_s)
       cookie.path = '/'
       cookie.domain = '.www.livescore.com'
@@ -41,21 +41,23 @@ module LivescoreParser
         if row.attr('class').match 'row-gray'
           index += 1
           time_raw = row.css('.min').text.strip
+          local_time = Time.parse(time_raw) + Time.new.utc_offset - @offset
+          local_date = local_time.hour >= 19 ? @date.clone - 1 : @date.clone
           {
             wiersz: index,
-            czas: (time_raw.match(/[0-9]+:[0-9]+/)) ? (Time.parse(time_raw) + Time.new.utc_offset - @offset).strftime('%H:%M') : time_raw,
+            czas: (time_raw.match(/[0-9]+:[0-9]+/)) ? local_time.strftime('%H:%M') : time_raw,
             gracz1: row.css('.ply.tright').text.strip,
             wynik1: row.css('.sco').text.strip.match(/^[0-9\?]+/).to_s,
             gracz2: row.css('.sco + .ply').text.strip,
             wynik2: row.css('.sco').text.strip.match(/[0-9\?]+$/).to_s,
-            data: @date.clone,
+            data: local_date.strftime('%B %d'),
             kraj: @country.clone,
             liga: @league.clone
           }
         elsif row.attr('class').match 'row-tall'
           @country = row.css('.left a:first').text.strip
           @league = row.css('.left a:last').text.strip
-          @date = row.css('.right').text.strip
+          @date = Date.parse(row.css('.right').text.strip)
           next
         end
       end.compact
